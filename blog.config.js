25c18afef881d2cf7a19c52cecab40ee7e5cb672
火炉旁的啸尘 <47934008+xiaochen1111@users.noreply.github.com>
2022-05-09 15:57:10 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
+  title: 'TryandTest',
+  author: 'Chuanfeng Li',
+  email: 'lcfjxzs@sina.com',
   link: 'https://nobelium.vercel.app',
   description: 'This gonna be an awesome website.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
